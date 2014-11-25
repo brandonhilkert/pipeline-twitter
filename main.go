b1b5ba997b078b6fcd9e90904cc8bc46f49c1bc9
Brandon Hilkert <brandonhilkert@gmail.com>
2014-11-25 09:31:15 -0500

    Cache tweets for 15 min.

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/go-martini/martini"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/martini-contrib/render"
+	"github.com/pmylund/go-cache"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
+)
+
+var (
+	c = cache.New(15*time.Minute, 1*time.Minute)
 )
 
 func main() {
-
 	m := martini.Classic()
 	m.Use(render.Renderer())
 
@@ -23,7 +30,31 @@ func main() {
 		return "up"
 	})
 
+	m.Get("/favicon.ico", func() string {
+		return "nope"
+	})
+
 	m.Get("/:screen_name", func(params martini.Params, r render.Render) {
+		screenName := params["screen_name"]
+
+		log.Println("Total number of cached users:", c.ItemCount())
+
+		if t, found := c.Get(screenName); found {
+			var tw []anaconda.Tweet
+
+			log.Println("Found tweets for", screenName)
+			err := json.Unmarshal(t.([]byte), &tw)
+
+			if err == nil {
+				newmap := map[string]interface{}{"response": tw}
+				r.JSON(200, newmap)
+			} else {
+				newmap := map[string]interface{}{"response": err}
+				r.JSON(400, newmap)
+			}
+
+			return
+		}
 
 		anaconda.SetConsumerKey(os.Getenv("TWITTER_KEY"))
 		anaconda.SetConsumerSecret(os.Getenv("TWITTER_SECRET"))
@@ -31,10 +62,12 @@ func main() {
 		api := anaconda.NewTwitterApi(os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_ACCESS_TOKEN_SECRET"))
 
 		v := url.Values{}
-		v.Set("screen_name", params["screen_name"])
+		v.Set("screen_name", screenName)
 		v.Set("count", "5")
 
 		tweets, err := api.GetUserTimeline(v)
+
+		go saveTweetsToCache(c, tweets, screenName)
 
 		if err == nil {
 			newmap := map[string]interface{}{"response": tweets}
@@ -46,4 +79,14 @@ func main() {
 	})
 
 	m.Run()
+}
+
+func saveTweetsToCache(c *cache.Cache, t []anaconda.Tweet, s string) {
+	j, err := json.Marshal(t)
+	if err != nil {
+		log.Println("Error marshaling tweets:", err)
+	}
+
+	c.Set(s, j, 0)
+	log.Println("Saved tweets for:", s)
 }
